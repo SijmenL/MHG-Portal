e3965e210b535c666ad42c9f9fed4d616ba3e745
Simon Klaren <Simon@klaren.nl>
2024-04-15 16:22:07 +0200

    add buttons for ordering
    leuk toegevoegd maar gaan we anders doen.... als je op een product klikt meteen een modal openen, daarna kan je of alleen jezelf kiezen of de users zodat je geen aantallen mee moet bijvoegen als je meerdere mensen kiest, maar die worden dan automatisch er achter gezet per user

--- resources/views/Speltakken/loodsen/loodsenbar/home.blade.php
+++ resources/views/Speltakken/loodsen/loodsenbar/home.blade.php
@@ -7,12 +7,30 @@
     .product-card
     {
         width: 40vw !important;
+        -webkit-user-select: none; /* Safari */
+        -ms-user-select: none; /* IE 10 and IE 11 */
+        user-select: none; /* Standard syntax */
+    }
+
+    .product-card > .card-body > p.card-text
+    {
+        margin-bottom: 0px;
     }
 
     a.card-link
     {
         text-decoration: none;
-    
+    }
+
+    .counter-btns
+    {
+        visibility: hidden;
+    }
+
+    .btn-choose-users
+    {
+        display: flex;
+        gap: 10px;
     }
 
 </style>
@@ -60,28 +78,49 @@
                             <a class="btn btn-info quick-action" >
                             {{-- <a class="btn btn-info quick-action" href="{{ route('loodsenbar.check-out') }}"> --}}
                                 <span class="material-symbols-rounded">Point_Of_Sale</span>
-                                <p>Check-out</p>
+                                <p>Check-out orders</p>
                             </a>
                         </div>
-                        <hr>
                     </div>
                     @endif
-                    <div id="loodsenbarHomeItems">
+                    {{-- choose users button --}}
+                    <div class="counter-btns">
+                        <div class="btn-choose-users w-100 mt-4">
+                            <button type="button" class="btn btn-warning w-50 me-2" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                                Choose users
+                            </button>
+                            <button class="btn btn-success w-50" type="button" href="#">
+                                Only myself
+                            </button>
+                            {{-- undo button --}}
+                        </div>
+                        <button class="btn btn-danger w-100 mt-2" type="button" onclick="resetCount()">
+                            Undo
+                        </button>
+                    </div>
+                    <hr>
+                    <div id="loodsenbarHomeItems" class="mb-5">
                         {{-- snel lopende producten --}}
                         <h2 class="text-center">Fast moving products</h2>
                         <div class="d-flex align-items-center flex-wrap justify-content-center">
                             @php if(count($products) == 0){print "No products found";} @endphp
                             @foreach ($products as $product) 
-                            <div class="p-1">
-                                <div class="card product-card">
-                                    <div class="card-body">
-                                        <h5 class="card-title">{{ $product->name }}</h5>
-                                        <p class="card-text">€ {{ $product->price }}</p>
-                                        <p class="card-text">category: {{ $product->category->name }}</p>
-                                        <p class="card-text">{{ $product->description }}</p>
+                            <a onclick="addProduct({{ $product->id }})" class="card-link">
+                                <div class="p-1">
+                                    <div class="card product-card">
+                                        <div class="card-body">
+                                            <h5 class="card-title">{{ $product->name }}</h5>
+                                            <p class="card-text">category: {{ $product->category->name }}</p>
+                                            <p class="card-text">{{ $product->description }}</p>
+                                            <p class="card-text">€ {{ $product->price }}</p>
+                                        </div>
+                                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary d-none" product-id="{{ $product->id }}" data-count="0">
+                                            0
+                                            <span class="visually-hidden">selected items</span>
+                                          </span>
                                     </div>
                                 </div>
-                            </div>
+                            </a>
                             @endforeach
                         </div>
                         <hr>
@@ -103,8 +142,67 @@
                             @endforeach
                         </div>
                     </div>
+
+                    
+                    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-scrollable">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        ...
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary">Understood</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <script>
+
+        function addProduct(id)
+        {
+            let badge = document.querySelector(`span[product-id="${id}"]`);
+            let count = parseInt(badge.getAttribute('data-count'));
+            count++;
+            badge.setAttribute('data-count', count);
+            badge.classList.remove('d-none');
+            badge.innerHTML = count;
+
+            // if count is > 0 show button
+            let button = document.querySelector('.counter-btns');
+            if(count > 0)
+            {
+                button.style.visibility = 'visible';
+            }else{
+                button.style.visibility = 'hidden';
+            }
+
+        }
+
+        function resetCount()
+        {
+            // reset all counts
+            let badges = document.querySelectorAll('span[product-id]');
+            badges.forEach(badge => {
+                badge.setAttribute('data-count', 0);
+                badge.classList.add('d-none');
+            });
+
+            // hide button
+            let button = document.querySelector('.counter-btns');
+            button.style.visibility = 'hidden';
+        }
+
+    </script>
 @endsection
